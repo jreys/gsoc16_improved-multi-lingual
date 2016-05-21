2b0affc00f1d0be5991bd7f097a9a95013f02b90
João Reys Santos <je_reyssantos@msn.com>
2016-05-21 10:19:52 +0100

    Adding initial skeleton for com_associations

---
+++ administrator/components/com_associations/controller.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_associations
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Component Controller
+ *
+ * @since  1.5
+ */
+class AssociationsController extends JControllerLegacy
+{
+    /**
+     * @var     string  The default view.
+     * @since   1.6
+     */
+    protected $default_view = '';
+
+    /**
+     * Method to display a view.
+     *
+     * @param   boolean  $cachable   If true, the view output will be cached
+     * @param   array    $urlparams  An array of safe url parameters and their variable types, for valid values see {@link JFilterInput::clean()}.
+     *
+     * @return  JController     This object to support chaining.
+     *
+     * @since   1.5
+     */
+    public function display($cachable = false, $urlparams = array())
+    {
+        return parent::display();
+    }
+}
---
+++ administrator/components/com_associations/helpers/associations.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_associations
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Content component helper.
+ *
+ * @since  1.6
+ */
+class AssociationsHelper extends JHelperContent
+{
+    public static $extension = 'com_associations';
+
+    /**
+     * Configure the Linkbar.
+     *
+     * @param   string  $vName  The name of the active view.
+     *
+     * @return  void
+     *
+     * @since   1.6
+     */
+    public static function addSubmenu($vName)
+    {
+        JHtmlSidebar::addEntry(
+            JText::_('JGLOBAL_ARTICLES'),
+            'index.php?option=com_content&view=articles',
+            $vName == 'articles'
+        );
+    }
+}
