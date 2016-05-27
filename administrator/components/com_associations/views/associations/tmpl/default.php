3eeab07caa2509f96429cb3ef768e28399fe25ed
João Reys Santos <je_reyssantos@msn.com>
2016-05-27 18:40:13 +0100

    Removed sidebar, added language string for Associations, initial mockup with no actions yet

--- administrator/components/com_associations/views/associations/tmpl/default.php
+++ administrator/components/com_associations/views/associations/tmpl/default.php
@@ -19,26 +19,74 @@ $app       = JFactory::getApplication();
 $user      = JFactory::getUser();
 $userId    = $user->get('id');
 
+$input = $app->input;
+
 $assoc = JLanguageAssociations::isEnabled();
 ?>
 
+<style type="text/css">
+
+		/* Fixed filter field in search bar */
+		.js-stools .js-stools-menutype {
+			float: left;
+			margin-right: 10px;
+			min-width: 220px;
+		}
+		html[dir=rtl] .js-stools .js-stools-menutype {
+			float: right;
+			margin-left: 10px
+			margin-right: 0;
+		}
+		.js-stools .js-stools-container-bar .js-stools-field-filter .chzn-container {
+			padding: 3px 0;
+		}
+	
+  </style>
+
 <form action="<?php echo JRoute::_('index.php?option=com_content&view=associations'); ?>" method="post" name="adminForm" id="adminForm">
 
-<?php if (!empty($this->sidebar)) : ?>
-	<div id="j-sidebar-container" class="span2">
-		<?php echo $this->sidebar; ?>
-	</div>
-	<div id="j-main-container" class="span10">
-<?php else : ?>
 	<div id="j-main-container">
-<?php endif;?>
 		<?php
 		// Search tools bar
-		echo JLayoutHelper::render('joomla.searchtools.default', array('view' => $this));
+		//echo JLayoutHelper::render('joomla.searchtools.default', array('view' => $this), null, array('debug' => false));
 		?>
-		
 
-		<?php echo $this->pagination->getListFooter(); ?>
+	<div class="js-stools clearfix">
+		<div class="clearfix">
+			<div class="js-stools-container-bar">
+
+				<div class="js-stools-field-filter js-stools-menutype">
+					<select id="component" name="component">
+						<option value="*">- Select Component -</option>
+						<option value="articles" selected="selected">Articles</option>
+					</select>
+				</div>
+
+				<div class="js-stools-field-filter js-stools-menutype">
+					<select id="category" name="category">
+						<option value="*">- Select Category -</option>
+						<option value="en-gb" selected="selected">Category (en-gb)</option>
+						<option value="uncategorized">Uncategorized</option>
+					</select>
+				</div>
+
+				<div class="js-stools-field-filter js-stools-menutype">
+					<select id="ref-language" name="ref-language">
+						<option value="*" selected="selected">- Select Reference Language -</option>
+						<option value="eng" >English</option>
+						<option value="fr">French</option>
+					</select>
+				</div>
+			<?php if ($assoc && $input->get('layout') != 'modal') : ?>
+				<a class="btn btn-primary" data-toggle="modal" role="button" href="index.php?option=com_content&view=article&layout=edit&id=1#articleSelectjform_associations_pt_PTModal" title="title">
+					<span class="icon-list "> </span> 
+					Select
+				</a>
+			<?php endif; ?>
+		 
+		
+		</div>
+	</div>
 
 		<input type="hidden" name="task" value="" />
 		<input type="hidden" name="boxchecked" value="0" />
