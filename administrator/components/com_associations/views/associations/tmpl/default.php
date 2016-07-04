101873da9858b8271d8dc710bfed4628e209ff8f
João Reys Santos <je_reyssantos@msn.com>
2016-07-04 11:21:47 +0100

    Found some code style problems, commit requested by André

--- administrator/components/com_associations/views/associations/tmpl/default.php
+++ administrator/components/com_associations/views/associations/tmpl/default.php
@@ -24,12 +24,13 @@ $input = $app->input;
 
 $assoc = JLanguageAssociations::isEnabled();
 
-// print_r($input->post);
+// For debugging this will be deleted -> print_r($input->post);
 
 $component = $input->post->get('component');
 $language = $input->post->get('ref-language');
 
-if (isset($component) && isset($language)) {
+if (isset($component) && isset($language))
+{
 	// Create a new query object.
 	$db = JFactory::getDbo();
 	$query = $db->getQuery(true);
@@ -75,7 +76,8 @@ if (isset($component) && isset($language)) {
 	$query->where($db->quoteName('a.language') . ' = ' . $db->quote($language));
 	$db->setQuery($query);
 	$results = $db->loadObjectList();
-	//print_r($results);
+
+	// Debug statement -> print_r($results);
 }
 
 ?>
@@ -132,84 +134,82 @@ if (isset($component) && isset($language)) {
 					<span class="icon-list "> </span> 
 					Select
 				</button>
-
 			<?php endif; ?>
-		 
-		
 		</div>
 	</div>
 
 	<?php if (!empty($results)) : ?>
-	<table class="table table-striped" id="contactList">
-		<thead>
-			<tr>
-				<th class="nowrap">
-					<?php echo JHtml::_('searchtools.sort', 'JGLOBAL_TITLE', 'a.name', $listDirn, $listOrder); ?>
-				</th>
-				<th width="15%" class="nowrap hidden-phone">
-					<?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_LANGUAGE', 'language_title', $listDirn, $listOrder); ?>
-				</th>
-				<?php if ($assoc) : ?>
-				<th width="5%" class="nowrap hidden-phone hidden-tablet">
-					<?php echo JHtml::_('searchtools.sort', 'COM_ASSOCIATIONS_HEADING_ASSOCIATION', 'association', $listDirn, $listOrder); ?>
-				</th>
-				<?php endif;?>
-				<th width="1%" class="nowrap hidden-phone">
-					<?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
-				</th>
-			</tr>
-		</thead>
-		<tfoot>
-			<tr>
-				<td colspan="10">
-					<?php //echo $this->pagination->getListFooter(); ?>
-				</td>
-			</tr>
-		</tfoot>
-		<tbody>
-		<?php
-		$n = count($this->items);
-		foreach ($results as $i => $item) :
-			$canCreate  = $user->authorise('core.create',     'com_menus.menu.' . $menutypeid);
-			$canEdit    = $user->authorise('core.edit',       'com_menus.menu.' . $menutypeid);
-			$canCheckin = $user->authorise('core.manage',     'com_checkin') || $item->checked_out == $user->get('id')|| $item->checked_out == 0;
-			$canChange  = $user->authorise('core.edit.state', 'com_menus.menu.' . $menutypeid) && $canCheckin;
-			?>
-			<tr class="row<?php echo $i % 2; ?>" sortable-group-id="<?php echo $item->catid; ?>">
-				<td class="nowrap has-context">
-					<div class="pull-left">
-						<?php if ($canEdit || $canEditOwn) : ?>
-							<a href="<?php echo JRoute::_('index.php?option=com_associations&view=association&layout=edit&acomponent=com_contact&aview=contact&id=' . (int) $item->id); ?>"><?php echo $this->escape($item->name); ?></a>
+		<table class="table table-striped" id="contactList">
+			<thead>
+				<tr>
+					<th class="nowrap">
+						<?php echo JHtml::_('searchtools.sort', 'JGLOBAL_TITLE', 'a.name', $listDirn, $listOrder); ?>
+					</th>
+					<th width="15%" class="nowrap hidden-phone">
+						<?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_LANGUAGE', 'language_title', $listDirn, $listOrder); ?>
+					</th>
+					<?php if ($assoc) : ?>
+					<th width="5%" class="nowrap hidden-phone hidden-tablet">
+						<?php echo JHtml::_('searchtools.sort', 'COM_ASSOCIATIONS_HEADING_ASSOCIATION', 'association', $listDirn, $listOrder); ?>
+					</th>
+					<?php endif;?>
+					<th width="1%" class="nowrap hidden-phone">
+						<?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
+					</th>
+				</tr>
+			</thead>
+			<tfoot>
+				<tr>
+					<td colspan="10">
+						<?php //echo $this->pagination->getListFooter(); ?>
+					</td>
+				</tr>
+			</tfoot>
+			<tbody>
+			<?php
+			$n = count($this->items);
+			foreach ($results as $i => $item) :
+				$canCreate  = $user->authorise('core.create',     'com_menus.menu.' . $menutypeid);
+				$canEdit    = $user->authorise('core.edit',       'com_menus.menu.' . $menutypeid);
+				$canCheckin = $user->authorise('core.manage',     'com_checkin') || $item->checked_out == $user->get('id')|| $item->checked_out == 0;
+				$canChange  = $user->authorise('core.edit.state', 'com_menus.menu.' . $menutypeid) && $canCheckin;
+				?>
+				<tr class="row<?php echo $i % 2; ?>" sortable-group-id="<?php echo $item->catid; ?>">
+					<td class="nowrap has-context">
+						<div class="pull-left">
+							<?php if ($canEdit || $canEditOwn) : ?>
+								<a href="<?php echo JRoute::_('index.php?option=com_associations&view=association&layout=edit&acomponent=com_contact&aview=contact&id=' . (int) $item->id); ?>"><?php echo $this->escape($item->name); ?></a>
+							<?php else : ?>
+								<?php echo $this->escape($item->name); ?>
+							<?php endif; ?>
+						</div>
+					</td>
+					<td class="small hidden-phone">
+						<?php if ($item->language == '*') : ?>
+							<?php echo JText::alt('JALL', 'language'); ?>
 						<?php else : ?>
-							<?php echo $this->escape($item->name); ?>
+							<?php echo $item->language_title ? JHtml::_('image', 'mod_languages/' . $item->language_image . '.gif', $item->language_title, array('title' => $item->language_title), true) . '&nbsp;' . $this->escape($item->language_title) : JText::_('JUNDEFINED'); ?>
 						<?php endif; ?>
-					</div>
-				</td>
-				<td class="small hidden-phone">
-					<?php if ($item->language == '*') : ?>
-						<?php echo JText::alt('JALL', 'language'); ?>
-					<?php else : ?>
-						<?php echo $item->language_title ? JHtml::_('image', 'mod_languages/' . $item->language_image . '.gif', $item->language_title, array('title' => $item->language_title), true) . '&nbsp;' . $this->escape($item->language_title) : JText::_('JUNDEFINED'); ?>
-					<?php endif; ?>
-				</td>
-				<?php if ($assoc) : ?>
-				<td class="hidden-phone hidden-tablet">
-					<?php if ($item->association) : ?>
-						<?php echo JHtml::_('contact.association', $item->id); ?>
-					<?php endif; ?>
-				</td>
-				<?php endif;?>
-				<td class="hidden-phone">
-					<?php echo $item->id; ?>
-				</td>
-			</tr>
-			<?php endforeach; ?>
-		</tbody>
-	</table>
-	<?php endif; ?>
+					</td>
+					<?php if ($assoc) : ?>
+					<td class="hidden-phone hidden-tablet">
+						<?php if ($item->association) : ?>
+							<?php echo JHtml::_('contact.association', $item->id); ?>
+						<?php endif; ?>
+					</td>
+					<?php endif;?>
+					<td class="hidden-phone">
+						<?php echo $item->id; ?>
+					</td>
+				</tr>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
 
-	<input type="hidden" name="task" value="" />
-	<input type="hidden" name="boxchecked" value="0" />
+	<?php endif; ?>
+	
+	<input type="hidden" name="task" value=""/>
+	<input type="hidden" name="boxchecked" value="0"/>
 	<?php echo JHtml::_('form.token'); ?>
 	</div>
 </form>
