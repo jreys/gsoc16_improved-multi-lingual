0899e5f5ed0f33d8f9b3183d2fe1d82eea2ede2a
João Reys Santos <je_reyssantos@msn.com>
2016-07-06 13:54:05 +0100

    Adding dynamic filter with real components

--- administrator/components/com_associations/models/fields/associatedcomponent.php
+++ administrator/components/com_associations/models/fields/associatedcomponent.php
@@ -39,37 +39,52 @@ class JFormFieldAssociatedComponent extends JFormFieldGroupedList
 	 */
 	protected function getGroups()
 	{
-		$options = array();
-		foreach ($this->articles as $key => $value)
-		{
-			$options[] = JHtml::_('select.option', $key, $value);
-		}
-		$options1 = array();
-		$options1['Content'] = $options;
-		// Merge any additional options in the XML definition.
-		$options = array_merge(parent::getGroups(), $options1);
+		jimport('joomla.filesystem.folder');
+		jimport('joomla.filesystem.file');
 
-		$componentsDirectory = getcwd() . "/components";
-		$components = scandir($componentsDirectory);
+		$options = array(array());
+
+		$componentsDirectory = JPATH_ADMINISTRATOR . "/components";
+		$frontendComponentsDirectory = JPATH_SITE . "/components";
+		$components          = scandir($componentsDirectory);
 
 		foreach ($components as $key => $value)
 		{
 			$currentDir = $componentsDirectory . "/" . $value . "/models/";
-			$componentModel = scandir($currentDir);
-			foreach ($componentModel as $key2 => $value2)
+
+			if (JFolder::exists($currentDir))
 			{
-				if (is_file($currentDir . $value2))
+				$componentModel = scandir($currentDir);
+
+				foreach ($componentModel as $key2 => $value2)
 				{
-					if(exec('grep '.escapeshellarg("JLanguageAssociations::isEnabled()").' '.$currentDir . $value2))
+					if (JFile::exists($currentDir . $value2))
 					{
-        				print_r($value . " has associations <br>");
-    				}
-    				break;
+						$file = file_get_contents($currentDir . $value2);
+
+						if (strpos($file, 'protected $associationsContext'))
+						{
+							JModelLegacy::addIncludePath(JPATH_ADMINISTRATOR . '/components/'
+								. $value . '/models', ucfirst(preg_replace('/\\.[^.\\s]{3,4}$/', '', $value2)) . 'Model');
+							/** @var XxxxxModelXxxx $model */
+							$model = JModelLegacy::getInstance(ucfirst($value2), ucfirst(substr($value, 4)) . 'Model', array('ignore_request' => true));
+
+							print_r($model);
+
+							$lang = JFactory::getLanguage();
+							$lang->load($value);
+							$options[JText::_($value)][] = JHtml::_('select.option', $key2, JText::_($value));
+							if (JFile::exists($frontendComponentsDirectory . "/" . $value . "/helpers/association.php"))
+							{
+								$options[JText::_($value)][] = JHtml::_('select.option', $key2, JText::_("JCategories"));
+							}
+						}
+					}
 				}
 			}
 		}
 
-		//print_r($components);
+		$options = array_merge(parent::getGroups(), $options);
 
 		return $options;
 	}
