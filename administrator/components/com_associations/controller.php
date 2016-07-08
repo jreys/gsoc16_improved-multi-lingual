a69c31f79b82133ff07b377caf23807b5316078f
João Reys Santos <je_reyssantos@msn.com>
2016-07-08 21:59:52 +0100

    Simplifying controller file

--- administrator/components/com_associations/controller.php
+++ administrator/components/com_associations/controller.php
@@ -34,26 +34,10 @@ class AssociationsController extends JControllerLegacy
 	 */
 	public function display($cachable = false, $urlparams = false)
 	{
-		// Get the document object.
-		$document	= JFactory::getDocument();
+		$view   = $this->input->get('view', 'associations');
+		$layout = $this->input->get('layout', 'associations');
+		$id     = $this->input->getInt('id');
 
-		// Set the default view name and format from the Request.
-		$vName   = $this->input->getCmd('view', 'associations');
-		$vFormat = $document->getType();
-		$lName   = $this->input->getCmd('layout', 'associations');
-
-		// Get the model and the view
-		$model = $this->getModel($vName);
-		$view = $this->getView($vName, $vFormat);
-
-		// Push the model into the view (as default).
-		$view->setModel($model, true);
-		$view->setLayout($lName);
-
-		// Push document object into the view.
-		$view->document = $document;
-
-		// Display the view
-		$view->display();
+		return parent::display();
 	}
 }
