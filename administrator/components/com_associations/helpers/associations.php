1e72320126b74706f07f6dc0e73348d0925b4b37
João Reys Santos <je_reyssantos@msn.com>
2016-07-08 13:50:47 +0100

    Adding helper function to load languages

--- administrator/components/com_associations/helpers/associations.php
+++ administrator/components/com_associations/helpers/associations.php
@@ -17,5 +17,61 @@ defined('_JEXEC') or die;
 class AssociationsHelper extends JHelperContent
 {
 	public static $extension = 'com_associations';
+	
+	/**
+	 * Method to load the language files for the components using associations.
+	 *
+	 * @return  void
+	 *
+	 * @since   3.7
+	 */
+	public static function loadLanguageFiles()
+	{
+		jimport('joomla.filesystem.folder');
+		jimport('joomla.filesystem.file');
+		$lang = JFactory::getLanguage();
+		
+		$componentsDirectory = JPATH_ADMINISTRATOR . "/components";
+		$frontendComponentsDirectory = JPATH_SITE . "/components";
+		$components          = scandir($componentsDirectory);
+		$frontendComponents  = scandir($frontendComponentsDirectory);
 
+		foreach ($components as $key => $value)
+		{
+			$currentDir = $componentsDirectory . "/" . $value . "/models/";
+
+			if (JFolder::exists($currentDir))
+			{
+				$componentModel = scandir($currentDir);
+
+				foreach ($componentModel as $key2 => $value2)
+				{
+					if (JFile::exists($currentDir . $value2))
+					{
+						$file = file_get_contents($currentDir . $value2);
+
+						if (strpos($file, 'protected $associationsContext'))
+						{
+							$lang->load($value, JPATH_ADMINISTRATOR, null, false, true)
+       							|| $lang->load($value, JPATH_ADMINISTRATOR . '/components/' . $value, null, false, true);
+       					}
+       				}
+       			}
+       		}
+       	}
+
+       	foreach ($frontendComponents as $key => $value)
+		{
+			if (JFile::exists($frontendComponentsDirectory . "/" . $value . "/helpers/association.php"))
+			{
+				$file = file_get_contents($frontendComponentsDirectory . "/" . $value . "/helpers/association.php");
+
+				if (strpos($file, 'getCategoryAssociations'))
+				{
+					$lang->load($value, JPATH_ADMINISTRATOR, null, false, true)
+       					|| $lang->load($value, JPATH_ADMINISTRATOR . '/components/' . $value, null, false, true);
+       			}
+       		}
+       	}
+	}
 }
