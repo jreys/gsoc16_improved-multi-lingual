e0da514ba878ab0b7fe5e07e3a5c4bab1b28701b
João Reys Santos <je_reyssantos@msn.com>
2016-07-12 16:40:51 +0100

    Fixing indentation to tabs instead of spaces

--- administrator/components/com_associations/views/associations/tmpl/default.php
+++ administrator/components/com_associations/views/associations/tmpl/default.php
@@ -9,8 +9,6 @@
 
 defined('_JEXEC') or die;
 
-JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
-
 JHtml::_('bootstrap.tooltip');
 JHtml::_('behavior.multiselect');
 JHtml::_('formbehavior.chosen', 'select');
@@ -25,119 +23,119 @@ $componentFilter = $this->state->get('associationcomponent');
 
 if ($componentFilter != '')
 {
-    $parts     = explode('.', $componentFilter);
-    $comp      = $parts[0];
-    $assocItem = $parts[1];
+	$parts     = explode('.', $componentFilter);
+	$comp      = $parts[0];
+	$assocItem = $parts[1];
 
-    JHtml::addIncludePath(JPATH_ADMINISTRATOR . '/components/' . $comp . '/helpers/html');
+	JHtml::addIncludePath(JPATH_ADMINISTRATOR . '/components/' . $comp . '/helpers/html');
 
-    // Get the value in the Association column
-    if ($comp == "com_content")
-    {
-        $assocValue = "contentadministrator.association";
-    }
-    elseif ($comp == "com_categories")
-    {
-        $assocValue = "categoriesadministrator.association";
-    }
-    elseif ($comp == "com_menus")
-    {
-        $assocValue = "MenusHtml.Menus.association";
-    }
-    else
-    {
-        $assocValue = $assocItem . '.association';
-    }
+	// Get the value in the Association column
+	if ($comp == "com_content")
+	{
+		$assocValue = "contentadministrator.association";
+	}
+	elseif ($comp == "com_categories")
+	{
+		$assocValue = "categoriesadministrator.association";
+	}
+	elseif ($comp == "com_menus")
+	{
+		$assocValue = "MenusHtml.Menus.association";
+	}
+	else
+	{
+		$assocValue = $assocItem . '.association';
+	}
 
-    // If it's not a category
-    if ($componentFilter != '' && !strpos($componentFilter, '|'))
-    {
-        $componentSplit = explode('.', $componentFilter);
-        $aComponent = $componentSplit[0];
-        $aView = $componentSplit[1];
-    }
-    elseif ($componentFilter != '') {
-        $componentSplit = explode('|', $componentFilter);
-        $aComponent = 'com_categories';
-        $aView = $componentSplit[1];
-    }
+	// If it's not a category
+	if ($componentFilter != '' && !strpos($componentFilter, '|'))
+	{
+		$componentSplit = explode('.', $componentFilter);
+		$aComponent = $componentSplit[0];
+		$aView = $componentSplit[1];
+	}
+	elseif ($componentFilter != '') {
+		$componentSplit = explode('|', $componentFilter);
+		$aComponent = 'com_categories';
+		$aView = $componentSplit[1];
+	}
 
-    if (isset($aComponent) && isset($aView))
-    {
-        $link = 'index.php?option=com_associations&view=association&layout=edit&acomponent='
-        . $aComponent . '&aview=' . $aView . '&id=';
-    }
+	if (isset($aComponent) && isset($aView))
+	{
+		$link = 'index.php?option=com_associations&view=association&layout=edit&acomponent='
+		. $aComponent . '&aview=' . $aView . '&id=';
+	}
 }
 ?>
 <form action="<?php echo JRoute::_('index.php?option=com_associations&view=associations'); ?>" method="post" name="adminForm" id="adminForm">
-    <div id="j-main-container">
+	<div id="j-main-container">
 
-    <?php echo JLayoutHelper::render('joomla.searchtools.default', array('view' => $this, 'options' => array('filterButton' => false))); ?>
+	<?php echo JLayoutHelper::render('joomla.searchtools.default', array('view' => $this, 'options' => array('filterButton' => false))); ?>
 
-    <?php if (empty($this->items)) : ?>
-        <div class="alert alert-no-items">
-            <?php echo JText::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
-        </div>
-    <?php else : ?>
-        <table class="table table-striped" id="contactList">
-            <thead>
-                <tr>
-                    <th width="1%" class="nowrap center">
-                        <?php echo JHtml::_('grid.checkall'); ?>
-                    </th>
-                    <th class="nowrap">
-                        <?php echo JHtml::_('searchtools.sort', 'JGLOBAL_TITLE', 'title', $listDirn, $listOrder); ?>
-                    </th>
-                    <th width="15%" class="nowrap hidden-phone">
-                        <?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_LANGUAGE', 'language_title', $listDirn, $listOrder); ?>
-                    </th>
-                    <th width="5%" class="nowrap">
-                        <?php echo JHtml::_('searchtools.sort', 'COM_ASSOCIATIONS_HEADING_ASSOCIATION', 'association', $listDirn, $listOrder); ?>
-                    </th>
-                    <th width="1%" class="nowrap hidden-phone">
-                        <?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-            <?php $n = count($this->items); ?>
-            <?php foreach ($this->items as $i => $item) : ?>
-                <tr class="row<?php echo $i % 2; ?>">
-                	<td class="center">
+	<?php if (empty($this->items)) : ?>
+		<div class="alert alert-no-items">
+			<?php echo JText::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
+		</div>
+	<?php else : ?>
+		<table class="table table-striped" id="contactList">
+			<thead>
+				<tr>
+					<th width="1%" class="nowrap center">
+						<?php echo JHtml::_('grid.checkall'); ?>
+					</th>
+					<th class="nowrap">
+						<?php echo JHtml::_('searchtools.sort', 'JGLOBAL_TITLE', 'title', $listDirn, $listOrder); ?>
+					</th>
+					<th width="15%" class="nowrap hidden-phone">
+						<?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_LANGUAGE', 'language_title', $listDirn, $listOrder); ?>
+					</th>
+					<th width="5%" class="nowrap">
+						<?php echo JHtml::_('searchtools.sort', 'COM_ASSOCIATIONS_HEADING_ASSOCIATION', 'association', $listDirn, $listOrder); ?>
+					</th>
+					<th width="1%" class="nowrap hidden-phone">
+						<?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+			<?php $n = count($this->items); ?>
+			<?php foreach ($this->items as $i => $item) : ?>
+				<tr class="row<?php echo $i % 2; ?>">
+					<td class="center">
 						<?php echo JHtml::_('grid.id', $i, $item->id); ?>
 					</td>
-                    <td class="nowrap has-context">
-                        <div class="pull-left">
-                            <a href="<?php echo JRoute::_($link . (int) $item->id); ?>"><?php echo $this->escape($item->title); ?></a>
-                        </div>
-                    </td>
-                    <td class="small hidden-phone">
-                        <?php echo $item->language_title ? JHtml::_('image', 'mod_languages/' . $item->language_image . '.gif', $item->language_title, array('title' => $item->language_title), true) . '&nbsp;' . $this->escape($item->language_title) : JText::_('JUNDEFINED'); ?>
-                    </td>
-                    <td>
-                        <?php if ($item->association) : ?>
-                            <?php echo JHtml::_($assocValue, $item->id); ?>
-                        <?php endif; ?>
-                    </td>
-                    <td class="hidden-phone">
-                        <?php echo $item->id; ?>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td>
-                        <?php echo $this->pagination->getListFooter(); ?>
-                    </td>
-                </tr>
-            </tfoot>
-        </table>
+					<td class="nowrap has-context">
+						<div class="pull-left">
+							<a href="<?php echo JRoute::_($link . (int) $item->id); ?>"><?php echo $this->escape($item->title); ?></a>
+						</div>
+					</td>
+					<td class="small hidden-phone">
+						<?php echo $item->language_title ? JHtml::_('image', 'mod_languages/' . $item->language_image . '.gif', $item->language_title, array('title' => $item->language_title), true) . '&nbsp;' . $this->escape($item->language_title) : JText::_('JUNDEFINED'); ?>
+					</td>
+					<td>
+						<?php if ($item->association) : ?>
+							<?php echo JHtml::_($assocValue, $item->id); ?>
+						<?php endif; ?>
+					</td>
+					<td class="hidden-phone">
+						<?php echo $item->id; ?>
+					</td>
+				</tr>
+				<?php endforeach; ?>
+			</tbody>
+			<tfoot>
+				<tr>
+					<td>
+						<?php echo $this->pagination->getListFooter(); ?>
+					</td>
+				</tr>
+			</tfoot>
+		</table>
 
-    <?php endif; ?>
+	<?php endif; ?>
 
-    <input type="hidden" name="task" value=""/>
-    <input type="hidden" name="boxchecked" value="0"/>
-    <?php echo JHtml::_('form.token'); ?>
-    </div>
+	<input type="hidden" name="task" value=""/>
+	<input type="hidden" name="boxchecked" value="0"/>
+	<?php echo JHtml::_('form.token'); ?>
+	</div>
 </form>
