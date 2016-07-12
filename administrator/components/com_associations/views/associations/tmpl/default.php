dc5cf8905bfc05ab02d6bb5eeff08d80e8cda381
João Reys Santos <je_reyssantos@msn.com>
2016-07-12 22:44:25 +0100

    Enabling sidebar on the template if it exists

--- administrator/components/com_associations/views/associations/tmpl/default.php
+++ administrator/components/com_associations/views/associations/tmpl/default.php
@@ -19,73 +19,81 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 
 ?>
 <form action="<?php echo JRoute::_('index.php?option=com_associations&view=associations'); ?>" method="post" name="adminForm" id="adminForm">
-	<div id="j-main-container">
 
-	<?php echo JLayoutHelper::render('joomla.searchtools.default', array('view' => $this, 'options' => array('filterButton' => false))); ?>
+<?php if (!empty( $this->sidebar)) : ?>
+    <div id="j-sidebar-container" class="span2">
+        <?php echo $this->sidebar; ?>
+    </div>
+	<div id="j-main-container" class="span10">
+<?php else : ?>
+    <div id="j-main-container">
+<?php endif;?>
 
-	<?php if (empty($this->items)) : ?>
-		<div class="alert alert-no-items">
-			<?php echo JText::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
+		<?php echo JLayoutHelper::render('joomla.searchtools.default', array('view' => $this, 'options' => array('filterButton' => false))); ?>
+
+		<?php if (empty($this->items)) : ?>
+			<div class="alert alert-no-items">
+				<?php echo JText::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
+			</div>
+		<?php else : ?>
+			<table class="table table-striped" id="contactList">
+				<thead>
+					<tr>
+						<th width="1%" class="nowrap center">
+							<?php echo JHtml::_('grid.checkall'); ?>
+						</th>
+						<th class="nowrap">
+							<?php echo JHtml::_('searchtools.sort', 'JGLOBAL_TITLE', 'title', $listDirn, $listOrder); ?>
+						</th>
+						<th width="15%" class="nowrap hidden-phone">
+							<?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_LANGUAGE', 'language_title', $listDirn, $listOrder); ?>
+						</th>
+						<th width="5%" class="nowrap">
+							<?php echo JHtml::_('searchtools.sort', 'COM_ASSOCIATIONS_HEADING_ASSOCIATION', 'association', $listDirn, $listOrder); ?>
+						</th>
+						<th width="1%" class="nowrap hidden-phone">
+							<?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
+						</th>
+					</tr>
+				</thead>
+				<tfoot>
+					<tr>
+						<td colspan="5">
+							<?php echo $this->pagination->getListFooter(); ?>
+						</td>
+					</tr>
+				</tfoot>
+				<tbody>
+				<?php foreach ($this->items as $i => $item) : ?>
+					<tr class="row<?php echo $i % 2; ?>">
+						<td class="center">
+							<?php echo JHtml::_('grid.id', $i, $item->id); ?>
+						</td>
+						<td class="nowrap has-context">
+							<div class="pull-left">
+								<a href="<?php echo JRoute::_($this->link . (int) $item->id); ?>"><?php echo $this->escape($item->title); ?></a>
+							</div>
+						</td>
+						<td class="small hidden-phone">
+							<?php echo $item->language_title ? JHtml::_('image', 'mod_languages/' . $item->language_image . '.gif', $item->language_title, array('title' => $item->language_title), true) . '&nbsp;' . $this->escape($item->language_title) : JText::_('JUNDEFINED'); ?>
+						</td>
+						<td>
+							<?php if ($item->association) : ?>
+								<?php echo JHtml::_($this->assocValue, $item->id); ?>
+							<?php endif; ?>
+						</td>
+						<td class="hidden-phone">
+							<?php echo $item->id; ?>
+						</td>
+					</tr>
+					<?php endforeach; ?>
+				</tbody>
+			</table>
+
+		<?php endif; ?>
+
+			<input type="hidden" name="task" value=""/>
+			<input type="hidden" name="boxchecked" value="0"/>
+			<?php echo JHtml::_('form.token'); ?>
 		</div>
-	<?php else : ?>
-		<table class="table table-striped" id="contactList">
-			<thead>
-				<tr>
-					<th width="1%" class="nowrap center">
-						<?php echo JHtml::_('grid.checkall'); ?>
-					</th>
-					<th class="nowrap">
-						<?php echo JHtml::_('searchtools.sort', 'JGLOBAL_TITLE', 'title', $listDirn, $listOrder); ?>
-					</th>
-					<th width="15%" class="nowrap hidden-phone">
-						<?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_LANGUAGE', 'language_title', $listDirn, $listOrder); ?>
-					</th>
-					<th width="5%" class="nowrap">
-						<?php echo JHtml::_('searchtools.sort', 'COM_ASSOCIATIONS_HEADING_ASSOCIATION', 'association', $listDirn, $listOrder); ?>
-					</th>
-					<th width="1%" class="nowrap hidden-phone">
-						<?php echo JHtml::_('searchtools.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
-					</th>
-				</tr>
-			</thead>
-			<tfoot>
-				<tr>
-					<td colspan="5">
-						<?php echo $this->pagination->getListFooter(); ?>
-					</td>
-				</tr>
-			</tfoot>
-			<tbody>
-			<?php foreach ($this->items as $i => $item) : ?>
-				<tr class="row<?php echo $i % 2; ?>">
-					<td class="center">
-						<?php echo JHtml::_('grid.id', $i, $item->id); ?>
-					</td>
-					<td class="nowrap has-context">
-						<div class="pull-left">
-							<a href="<?php echo JRoute::_($this->link . (int) $item->id); ?>"><?php echo $this->escape($item->title); ?></a>
-						</div>
-					</td>
-					<td class="small hidden-phone">
-						<?php echo $item->language_title ? JHtml::_('image', 'mod_languages/' . $item->language_image . '.gif', $item->language_title, array('title' => $item->language_title), true) . '&nbsp;' . $this->escape($item->language_title) : JText::_('JUNDEFINED'); ?>
-					</td>
-					<td>
-						<?php if ($item->association) : ?>
-							<?php echo JHtml::_($this->assocValue, $item->id); ?>
-						<?php endif; ?>
-					</td>
-					<td class="hidden-phone">
-						<?php echo $item->id; ?>
-					</td>
-				</tr>
-				<?php endforeach; ?>
-			</tbody>
-		</table>
-
-	<?php endif; ?>
-
-	<input type="hidden" name="task" value=""/>
-	<input type="hidden" name="boxchecked" value="0"/>
-	<?php echo JHtml::_('form.token'); ?>
-	</div>
 </form>
