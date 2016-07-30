60a77c581df35130376123d2c211eb2b601b0ba2
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-07-30 19:56:04 +0100

    add checkin to the controller PoC

--- administrator/components/com_associations/controllers/associations.php
+++ administrator/components/com_associations/controllers/associations.php
@@ -23,7 +23,7 @@ class AssociationsControllerAssociations extends JControllerAdmin
 	 *
 	 * @since  __DEPLOY_VERSION__
 	 */
-	protected $view_list = 'Associations';
+	protected $view_list = 'associations';
 
 	/**
 	 * Proxy for getModel.
@@ -39,5 +39,47 @@ class AssociationsControllerAssociations extends JControllerAdmin
 	public function getModel($name = 'Associations', $prefix='AssociationsModel', $config = array('ignore_request' => true))
 	{
 		return parent::getModel($name, $prefix, $config);
+	}
+
+
+	/**
+	 * Check in of one or more records.
+	 *
+	 * @return  boolean  True on success
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function checkin()
+	{
+		// Check for request forgeries.
+		JSession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
+
+		$ids    = JFactory::getApplication()->input->post->get('cid', array(), 'array');
+		$model  = $this->getModel();
+		$cp     = AssociationsHelper::getComponentProperties($model->getState('component'));
+		$return = false;
+
+		// Only check in, if component allows to check out.
+		if (!is_null($cp->fields->checked_out))
+		{
+			$return = $cp->model->checkin($ids);
+
+			// Checkin failed.
+			if ($return === false)
+			{
+				$message     = JText::sprintf('JLIB_APPLICATION_ERROR_CHECKIN_FAILED', $model->getError());
+				$messageType = 'error';
+			}
+			// Checkin succeeded.
+			else
+			{
+				$message     = JText::plural($this->text_prefix . '_N_ITEMS_CHECKED_IN', count($ids));
+				$messageType = 'message';
+			}
+
+			$this->setRedirect(JRoute::_('index.php?option=' . $this->option . '&view=' . $this->view_list), false), $message,	$messageType);
+		}
+
+		return (boolean) $return;
 	}
 }
