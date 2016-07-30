5a5f48eca1d264e6bd562f4133dda7146dd3e0c0
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-07-30 19:53:09 +0100

    add checked out tiem field

--- administrator/components/com_associations/helpers/associations.php
+++ administrator/components/com_associations/helpers/associations.php
@@ -213,22 +213,23 @@ class AssociationsHelper extends JHelperContent
 
 			// Component fields
 			// @todo This need should be checked hardcoding.
-			$cp[$key]->fields              = new Registry;
-			$cp[$key]->fields->title       = isset($cp[$key]->tableFields['name']) ? 'name' : null;
-			$cp[$key]->fields->title       = isset($cp[$key]->tableFields['title']) ? 'title' : $cp[$key]->fields->title;
-			$cp[$key]->fields->alias       = isset($cp[$key]->tableFields['alias']) ? 'alias' : null;
-			$cp[$key]->fields->ordering    = isset($cp[$key]->tableFields['ordering']) ? 'ordering' : null;
-			$cp[$key]->fields->ordering    = isset($cp[$key]->tableFields['lft']) ? 'lft' : $cp[$key]->fields->ordering;
-			$cp[$key]->fields->menutype    = isset($cp[$key]->tableFields['menutype']) ? 'menutype' : null;
-			$cp[$key]->fields->level       = isset($cp[$key]->tableFields['level']) ? 'level' : null;
-			$cp[$key]->fields->catid       = isset($cp[$key]->tableFields['catid']) ? 'catid' : null;
-			$cp[$key]->fields->language    = isset($cp[$key]->tableFields['language']) ? 'language' : null;
-			$cp[$key]->fields->access      = isset($cp[$key]->tableFields['access']) ? 'access' : null;
-			$cp[$key]->fields->published   = isset($cp[$key]->tableFields['state']) ? 'state' : null;
-			$cp[$key]->fields->published   = isset($cp[$key]->tableFields['published']) ? 'published' : $cp[$key]->fields->published;
-			$cp[$key]->fields->created_by  = isset($cp[$key]->tableFields['created_user_id']) ? 'created_user_id' : null;
-			$cp[$key]->fields->created_by  = isset($cp[$key]->tableFields['created_by']) ? 'created_by' : $cp[$key]->fields->created_by;
-			$cp[$key]->fields->checked_out = isset($cp[$key]->tableFields['checked_out']) ? 'checked_out' : null;
+			$cp[$key]->fields                   = new Registry;
+			$cp[$key]->fields->title            = isset($cp[$key]->tableFields['name']) ? 'name' : null;
+			$cp[$key]->fields->title            = isset($cp[$key]->tableFields['title']) ? 'title' : $cp[$key]->fields->title;
+			$cp[$key]->fields->alias            = isset($cp[$key]->tableFields['alias']) ? 'alias' : null;
+			$cp[$key]->fields->ordering         = isset($cp[$key]->tableFields['ordering']) ? 'ordering' : null;
+			$cp[$key]->fields->ordering         = isset($cp[$key]->tableFields['lft']) ? 'lft' : $cp[$key]->fields->ordering;
+			$cp[$key]->fields->menutype         = isset($cp[$key]->tableFields['menutype']) ? 'menutype' : null;
+			$cp[$key]->fields->level            = isset($cp[$key]->tableFields['level']) ? 'level' : null;
+			$cp[$key]->fields->catid            = isset($cp[$key]->tableFields['catid']) ? 'catid' : null;
+			$cp[$key]->fields->language         = isset($cp[$key]->tableFields['language']) ? 'language' : null;
+			$cp[$key]->fields->access           = isset($cp[$key]->tableFields['access']) ? 'access' : null;
+			$cp[$key]->fields->published        = isset($cp[$key]->tableFields['state']) ? 'state' : null;
+			$cp[$key]->fields->published        = isset($cp[$key]->tableFields['published']) ? 'published' : $cp[$key]->fields->published;
+			$cp[$key]->fields->created_by       = isset($cp[$key]->tableFields['created_user_id']) ? 'created_user_id' : null;
+			$cp[$key]->fields->created_by       = isset($cp[$key]->tableFields['created_by']) ? 'created_by' : $cp[$key]->fields->created_by;
+			$cp[$key]->fields->checked_out      = isset($cp[$key]->tableFields['checked_out']) ? 'checked_out' : null;
+			$cp[$key]->fields->checked_out_time = isset($cp[$key]->tableFields['checked_out_time']) ? 'checked_out_time' : null;
 
 			// Disallow ordering according to component.
 			$cp[$key]->excludeOrdering = array();
