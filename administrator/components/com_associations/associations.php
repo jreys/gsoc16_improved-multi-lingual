8ff32ac69ba74e9fb53027ff183be21a2707dba7
João Reys Santos <je_reyssantos@msn.com>
2016-08-01 07:56:14 +0100

    Adding ACL for direct access in list view

--- administrator/components/com_associations/associations.php
+++ administrator/components/com_associations/associations.php
@@ -15,6 +15,17 @@ if (!JFactory::getUser()->authorise('core.manage', 'com_associations'))
 	return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
 }
 
+$input     = JFactory::getApplication()->input;
+$component = $input->get('component', '', 'string');
+$splitcpnt = explode('.', $component);
+$component = $splitcpnt[0];
+
+// Check if user has permission to access the component
+if ($component != '' && !JFactory::getUser()->authorise('core.manage', $component))
+{
+    throw new Exception(JText::_('JERROR_ALERTNOAUTHOR'), 403);
+}
+
 JLoader::register('AssociationsHelper', __DIR__ . '/helpers/associations.php');
 
 $controller = JControllerLegacy::getInstance('Associations');
