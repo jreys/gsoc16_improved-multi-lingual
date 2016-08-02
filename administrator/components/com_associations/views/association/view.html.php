f26c39b41c8924e0f047e76d520fda511a2a2e19
João Reys Santos <je_reyssantos@msn.com>
2016-08-02 23:18:58 +0100

    CS

--- administrator/components/com_associations/views/association/view.html.php
+++ administrator/components/com_associations/views/association/view.html.php
@@ -151,13 +151,13 @@ class AssociationsViewAssociation extends JViewLegacy
 
 		$bar = JToolbar::getInstance('toolbar');
 		$bar->appendButton(
-			'Custom', '<button onclick="Joomla.submitbutton(\'reference\')"
-			class="btn btn-small btn-success"><span class="icon-apply icon-white"></span>'
+			'Custom', '<button onclick="Joomla.submitbutton(\'reference\')"'
+			. 'class="btn btn-small btn-success"><span class="icon-apply icon-white"></span>'
 			. JText::_('COM_ASSOCIATIONS_SAVE_REFERENCE') . '</button>', 'reference'
 		);
 		$bar->appendButton(
-			'Custom', '<button onclick="Joomla.submitbutton(\'target\')"
-			class="btn btn-small btn-success"><span class="icon-apply icon-white"></span>' 
+			'Custom', '<button onclick="Joomla.submitbutton(\'target\')"'
+			. 'class="btn btn-small btn-success"><span class="icon-apply icon-white"></span>' 
 			. JText::_('COM_ASSOCIATIONS_SAVE_TARGET') . '</button>', 'target'
 		);
 
