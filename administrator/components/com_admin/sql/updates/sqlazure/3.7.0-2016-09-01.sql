052ed1a93dce500ef2d53c127a085205fe00388c
João Reys Santos <je_reyssantos@msn.com>
2016-08-09 19:21:41 +0100

    Adding menus on update

--- administrator/components/com_admin/sql/updates/sqlazure/3.7.0-2016-09-01.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/3.7.0-2016-09-01.sql
@@ -4,3 +4,6 @@ INSERT INTO [#__extensions] ([extension_id], [name], [type], [element], [folder]
 SELECT 33, 'com_associations', 'component', 'com_associations', '', 1, 1, 1, 1, '', '', '', '', 0, '1900-01-01 00:00:00', 0, 0;
 
 SET IDENTITY_INSERT [#__extensions] OFF;
+
+INSERT INTO [#__menu] ([menutype], [title], [alias], [note], [path], [link], [type], [published], [parent_id], [level], [component_id], [checked_out], [checked_out_time], [browserNav], [access], [img], [template_style_id], [params], [lft], [rgt], [home], [language], [client_id])
+SELECT 'menu', 'com_associations', 'Multilingual Associations', '', 'Multilingual Associations', 'index.php?option=com_associations', 'component', 1, 1, 1, 33, 0, '1900-01-01 00:00:00', 0, 1, 'class:associations', 0, '', 43, 44, 0, '*', 1
