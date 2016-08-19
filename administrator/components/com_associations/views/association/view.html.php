60ef78b654be50272fcbda859d00c879d1dffaef
João Reys Santos <je_reyssantos@msn.com>
2016-08-19 11:32:26 +0100

    Correction

--- administrator/components/com_associations/views/association/view.html.php
+++ administrator/components/com_associations/views/association/view.html.php
@@ -157,7 +157,7 @@ class AssociationsViewAssociation extends JViewLegacy
 		// Hide main menu.
 		JFactory::getApplication()->input->set('hidemainmenu', 1);
 
-		JToolbarHelper::title(JText::sprintf('COM_ASSOCIATIONS_HEADER_EDIT', $this->itemType->componentTitle, $this->itemType->title), 'edit');
+		JToolbarHelper::title(JText::sprintf('COM_ASSOCIATIONS_HEADER_EDIT', $this->itemType->componentTitle, $this->itemType->title), 'contract');
 
 		$bar = JToolbar::getInstance('toolbar');
 
